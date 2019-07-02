bd5fc208e7edf271ea358e1044a87fe711e73194
rrosztoczy <rrosztoczy@gmail.com>
2019-07-02 09:53:19 -0600

    playing around

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,12 +13,20 @@ def sort_array_char_count(array)
 end
   #swap_elements
 def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+end
+def swap_elements_to(array, index, final_index)
+    array[index], array[final_index] = array[final_index], array[index]
 end
   #reverse_array
 def reverse_array(array)
+    reversed_array = array.reverse
 end
   #kesha_maker
-def kesha_maker
+def kesha_maker(array)
+    array.each do |string|
+        string[2] = '$'
+    end
 end
   #find_a
 def find_a
