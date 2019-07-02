e50d462691fd28c9e28b43a84530872e6220b0ea
rrosztoczy <rrosztoczy@gmail.com>
2019-07-02 09:37:18 -0600

    first three passed

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
 
+  #sort_array_asc
+def sort_array_asc(array)
+    sorted_arr = array.sort
+end
+  #sort_array_desc
+def sort_array_desc(array)
+    sorted_arr = array.sort {|a, b| b <=> a}
+end
+  #sort_array_char_count
+def sort_array_char_count(array)
+    sorted_arr = array.sort {|a, b| a.length <=> b.length}
+end
+  #swap_elements
+def swap_elements(array)
+end
+  #reverse_array
+def reverse_array(array)
+end
+  #kesha_maker
+def kesha_maker
+end
+  #find_a
+def find_a
+end
+  #sum_array
+def sum_array
+end
+  #add_s
+def add_s
+end
